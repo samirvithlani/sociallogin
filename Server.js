ba749872bfa8b02a21e05e77adaff576be6e34c8
=samirvithlani <=samir.vithlani83955@gmail.com>
2020-07-20 16:53:57 +0530

    a

--- Server.js
+++ Server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path'); 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/SIGNINDEMO'));
+app.use(express.static(__dirname + '/dist/signindemo'));
 // Start the app by listening on the default
 // Heroku port
 
 console.log("file called...");
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/SIGNINDEMO/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/signindemo/index.html'));   
 });  
 app.listen(process.env.PORT || 8080);
